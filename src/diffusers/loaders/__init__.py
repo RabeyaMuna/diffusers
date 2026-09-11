a108bc5383110c49a369964deeecf84f15ef7ec8
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-11 16:23:49 -0400

    RabeyaMuna__diff__id_22

--- src/diffusers/loaders/__init__.py
+++ src/diffusers/loaders/__init__.py
@@ -3,6 +3,9 @@ from typing import TYPE_CHECKING
 from ..utils import DIFFUSERS_SLOW_IMPORT, _LazyModule, deprecate
 from ..utils.import_utils import is_peft_available, is_torch_available, is_transformers_available
 
+if is_transformers_available():
+    from .single_file import FromSingleFileMixin
+
 
 def text_encoder_lora_state_dict(text_encoder):
     deprecate(
